8925e03153befd2e7c4f7a9cfa378c1adbbd420a
Velotist <velotist@aikq.de>
2019-03-23 19:15:33 +0100

    Projektdateien hinzufügen.

---
+++ ShowMeASlogan/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+
+namespace ShowMeASlogan
+{
+    public partial class MainWindow : Window
+    {
+        // wichtig für den Aufbau einer Verbindung zum SQL Server bzw. zur Datenbank
+        SqlConnection sqlCon;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // über welche Datenverbindung soll die Anwendung eine Verbindung mit der Datenbank erstellen?
+
+            string conString = ConfigurationManager.ConnectionStrings["ShowMeASlogan.Properties.Settings.SlogansConnectionString"].ConnectionString;
+            sqlCon = new SqlConnection(conString);
+        }
+
+        public void ShowSlogans()
+        {
+            DataTable dataSlogan = new DataTable();
+            DataSet dataSet = new DataSet();
+            
+            Random rdnum = new Random();
+            int raNum = rdnum.Next(1, 58);
+
+            string query = "SELECT * FROM SlogansTable WHERE Id = " + raNum;
+            SqlDataAdapter sqlData = new SqlDataAdapter(query, sqlCon);
+
+            using(sqlData)
+            {  
+                sqlData.Fill(dataSlogan);
+                libx_Data.DisplayMemberPath = "Slogan";
+                libx_Data.SelectedValuePath = "Id";
+                libx_Data.ItemsSource = dataSlogan.DefaultView;
+            }
+        }
+
+        private void Btn_Show_Click(object sender, RoutedEventArgs e)
+        {
+            ShowSlogans();
+        }
+    }
+}
